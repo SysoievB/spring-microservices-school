244b8ca9dc68d7c3be1f5842689883a18197be39
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-08 05:52:09 +0200

    Added microcervices project

--- student-service/src/main/java/com/studentservice/StudentService.java
+++ student-service/src/main/java/com/studentservice/StudentService.java
@@ -3,13 +3,13 @@ package com.studentservice;
 import com.studentservice.dto.School;
 import com.studentservice.dto.StudentResponse;
 import lombok.AllArgsConstructor;
+import lombok.val;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -17,19 +17,19 @@ public class StudentService {
     private final StudentRepository studentRepository;
     private final RestTemplate restTemplate;
 
-    public ResponseEntity<?> createStudent(Student student){
-        try{
+    public ResponseEntity<?> createStudent(Student student) {
+        try {
             return new ResponseEntity<Student>(studentRepository.save(student), HttpStatus.OK);
-        }catch(Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    public ResponseEntity<?> fetchStudentById(String id){
-        Optional<Student> student =  studentRepository.findById(id);
+    public ResponseEntity<?> fetchStudentById(String id) {
+        val student =  studentRepository.findById(id);
         if(student.isPresent()){
-            School school = restTemplate.getForObject("http://localhost:8080/school/" + student.get().getSchoolId(), School.class);
-            StudentResponse studentResponse = new StudentResponse(
+            val school = restTemplate.getForObject("http://localhost:8080/school/" + student.get().getSchoolId(), School.class);
+            val studentResponse = new StudentResponse(
                     student.get().getId(),
                     student.get().getName(),
                     student.get().getAge(),
@@ -42,12 +42,12 @@ public class StudentService {
         }
     }
 
-    public ResponseEntity<?> fetchStudents(){
-        List<Student> students = studentRepository.findAll();
-        if(!students.isEmpty()){
+    public ResponseEntity<?> fetchStudents() {
+        val students = studentRepository.findAll();
+        if (!students.isEmpty()) {
             return new ResponseEntity<List<Student>>(students, HttpStatus.OK);
-        }else {
-            return new ResponseEntity<>("No Students",HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>("No Students", HttpStatus.NOT_FOUND);
         }
     }
 
